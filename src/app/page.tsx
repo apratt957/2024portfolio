bdf5af291d2e19ab22bd4661405d7cec4ec85cb6
Andrew Pratt <apratt957@gmail.com>
2024-01-27 07:37:05 -0600

    remove padding so that card cannot go off page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-'use client';
+'use client'
 
 import { motion } from "framer-motion"
-import styles from "./page.module.css";
+import { useRef } from "react"
+import styles from "./page.module.css"
 
 export default function Home() {
+
+  const constraintsRef = useRef(null)
+
   return (
-    <main className={styles.main}>
-      <motion.div drag className={styles.card}>A card</motion.div>
-    </main>
+    <motion.main ref={constraintsRef}  className={styles.main}>
+      <motion.div drag dragConstraints={constraintsRef} className={styles.card}>A card</motion.div>
+    </motion.main>
   );
 }
