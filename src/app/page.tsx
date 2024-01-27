fa7d051fe8c5e94c5d57b5e3d6f7a283d372de34
Andrew Pratt <apratt957@gmail.com>
2024-01-27 06:55:21 -0600

    add basic draggable card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+
+import { motion } from "framer-motion"
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <motion.div drag className={styles.card}>A card</motion.div>
     </main>
   );
 }
