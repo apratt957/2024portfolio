2bb779b1c92908ee571a34e267d1ec4a9f5891c7
Andrew Pratt <apratt957@gmail.com>
2024-02-26 15:44:32 -0600

    remove unnecessary layouts and add back button

--- src/app/(work)/keeping-time/page.tsx
+++ src/app/(work)/keeping-time/page.tsx
@@ -1,22 +1,37 @@
-import Header from "../Header";
-import WorkLayout from "../layout";
+"use client";
+
 import styles from "../page.module.css";
 import { work } from "@/app/work";
 import { findCurrentProject } from "@/helpers";
+import Image from "next/image";
 
 export default function KeepingTime() {
+  const handleBackButtonClick = () => {
+    window.history.back();
+  };
+
   const projectName = "Keeping Time";
   const currentProject = findCurrentProject(projectName, work) as {
+    title: string;
+    description: string;
     backgroundColor: string;
   };
   return (
-    <WorkLayout>
-      <div className={styles.container}>
-        <Header backgroundColor={currentProject.backgroundColor} color="white">
-          Keeping Time
-        </Header>
-        <div className={styles.content}>some random info</div>
+    <div className={styles.container}>
+      <div className={styles.back} onClick={handleBackButtonClick}>
+        <Image src="/back.svg" alt="back" height={80} width={80} />
       </div>
-    </WorkLayout>
+      <header
+        className={styles.workHeader}
+        style={{
+          backgroundColor: currentProject.backgroundColor,
+          color: "white",
+        }}
+      >
+        <h1>{currentProject.title}</h1>
+        <div>{currentProject.description}</div>
+      </header>
+      <div className={styles.content}>some random info</div>
+    </div>
   );
 }
